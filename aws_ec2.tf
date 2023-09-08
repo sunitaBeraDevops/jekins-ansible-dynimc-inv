bc4cc29c5a70cdaa0de3231303093708d6cc1b56
sunitaBeraDevops <139767645+sunitaBeraDevops@users.noreply.github.com>
2023-09-08 22:01:17 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,13 +1,13 @@
 provider "aws"{
 region = "ap-south-1"
-}
 
-resource "aws_instance" "AWSServer"{
-ami = "ami-0da59f1af71ea4ad2"
+}
+resource "aws_instance" "AWS Server" {
+ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
-key_name = "devopsmithuntechnologiesbhaskar"
 security_groups = ["launch-wizard-2"]
+key_name = "Devops"
 tags = {
-Name = "tomcatservers"
+Name = "TomcatServers"
 }
 }
