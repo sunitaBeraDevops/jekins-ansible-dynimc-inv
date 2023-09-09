73129ffbe7cc6c5496de37e08d543864491cca6d
sunitaBeraDevops <139767645+sunitaBeraDevops@users.noreply.github.com>
2023-09-09 21:35:34 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region = "ap-south-1"
 
 }
-resource "aws_instance" "AWS Server" {
+resource "aws_instance" "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
 security_groups = ["launch-wizard-2"]
